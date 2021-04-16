a14fd872415584de99169d21942b16d9d67f0bf4
Dmitry Kulyatin <redsanchezz@yandex.ru>
2021-04-16 16:47:34 +0300

    'add_color_inp'

--- app/js/script.js
+++ app/js/script.js
@@ -9,4 +9,18 @@ function sortBtn(){
     })
 
 }
+function colorInputs(){
+    let $colorList= $('.catalog__filter-colors-list');
+    let labels =  Array.from($('label', $colorList));
+    let inputs = Array.from($('input', $colorList));
+
+    inputs.map((value, i) => {
+        inputs[i].addEventListener('change', (e) => {
+            labels[i].classList.toggle('active');
+        })
+    })
+}
+
 sortBtn();
+colorInputs();
+
